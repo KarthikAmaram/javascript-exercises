51598e56560f9c1771da7e58ee67de5c9b31de5e
Karthik Amaram <amaramkart09@gmail.com>
2025-06-01 23:26:00 -0500

    Finished the lesson

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const letters = string.split("");
+    const backwardsLetters = letters.reverse();
+    return backwardsLetters.join("");
 };
 
 // Do not edit below this line
