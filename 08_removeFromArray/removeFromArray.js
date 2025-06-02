83f26a8b8cf22dd425f0bc4737dabb4bde2d7b4a
Karthik Amaram <amaramkart09@gmail.com>
2025-06-02 01:13:34 -0500

    Finished the lesson

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value1, value2, value3, value4) {
+    let newArray = array;
+    if (array.includes(value1)) {
+        newArray = newArray.filter((element) => element !== value1);
+    }
+    if (array.includes(value2)) {
+        newArray = newArray.filter((element) => element !== value2);
+    }
+    if (array.includes(value3)) {
+        newArray = newArray.filter((element) => element !== value3);
+    }
+    if (array.includes(value4)) {
+        newArray = newArray.filter((element) => element !== value4);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
