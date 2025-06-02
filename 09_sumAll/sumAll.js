63a5881fc9c8fe7a27c20df1a9ffb93990e2983b
Karthik Amaram <amaramkart09@gmail.com>
2025-06-02 01:29:08 -0500

    Finished the lesson

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(startValue, endValue) {
+    if (startValue < 0 || endValue < 0){
+        return "ERROR";
+    }
+    if (!(Number.isInteger(startValue)) || !(Number.isInteger(endValue))){
+        return "ERROR";
+    }
+    let actualStartValue = startValue;
+    let actualEndValue = endValue;
+    if (startValue > endValue){
+        actualStartValue = endValue;
+        actualEndValue = startValue;
+    }
+    let sum = 0;
+    for (let i = actualStartValue; i <= actualEndValue; i++) {
+        sum += i
+    };
+    return sum;
 };
 
 // Do not edit below this line
