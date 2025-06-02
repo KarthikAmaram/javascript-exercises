7a825a8420898120f5c4f5f80181ff9f79ec6d74
Karthik Amaram <amaramkart09@gmail.com>
2025-06-02 08:46:59 -0500

    Finished the lesson

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp - 32) * (5/9);
+  let roundedTemp = Math.round(celsiusTemp * 10)/10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp * (9/5) + 32);
+  let roundedTemp = Math.round(fahrenheitTemp * 10)/10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
