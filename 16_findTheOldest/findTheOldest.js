df376de35366c340d08b87ac4166b12af3304ba9
Karthik Amaram <amaramkart09@gmail.com>
2025-06-03 10:12:32 -0500

    Finished it

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        let aDeath = new Date().getFullYear();
+        let bDeath = new Date().getFullYear();
 
+        if (a.yearOfDeath){
+            aDeath = a.yearOfDeath;
+        }
+        if (b.yearOfDeath){
+            bDeath = b.yearOfDeath;
+        }
+        aAge = aDeath - a.yearOfBirth;
+        bAge = bDeath - b.yearOfBirth;
+        if (aAge > bAge) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
