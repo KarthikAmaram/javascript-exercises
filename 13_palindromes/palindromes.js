559989cd7990a4c82a4a1e81bb661aac0fd5a2d7
Karthik Amaram <amaramkart09@gmail.com>
2025-06-03 09:47:09 -0500

    Finished it

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    smallString =  string.toLowerCase();
+    const filteredString = smallString.split("");
+    const splitString = smallString.split("");
+    for (let i = filteredString.length - 1; i >= 0; i--) {
+        if (filteredString[i] === "!" || filteredString[i] === "." || filteredString[i] === "," || filteredString[i] === " ") {
+            filteredString.splice(i, 1);
+        }
+        
+    }
+    for (let i = splitString.length - 1; i >= 0; i--) {
+        if (splitString[i] === "!" || splitString[i] === "." || splitString[i] === "," || splitString[i] === " ") {
+            splitString.splice(i, 1);
+        }
+        
+    }
+    string = filteredString.join("");
+    const backwardsString = String(splitString.reverse().join(""));
+    if (backwardsString === string){
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes("racecar!")
 
 // Do not edit below this line
 module.exports = palindromes;
