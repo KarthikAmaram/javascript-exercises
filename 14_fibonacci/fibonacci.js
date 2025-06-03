a5350d150d3c188a2278df062c4e99b59db28baa
Karthik Amaram <amaramkart09@gmail.com>
2025-06-03 09:58:43 -0500

    Finished it

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0){
+        return 0;
+    }
+    if (num < 0) {
+        return "OOPS";
+    }
+    num = Number(num);
+    let fibonacciSequence = [1, 1];
+    for (let i = 1; i < num; i++) {
+        nextValue = fibonacciSequence[i] + fibonacciSequence[i-1];
+        fibonacciSequence.push(nextValue);    
+    }
+    return fibonacciSequence[num - 1];
 };
 
 // Do not edit below this line
