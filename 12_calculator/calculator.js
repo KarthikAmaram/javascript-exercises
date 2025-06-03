f905126521c03a171c67a503e315541c982fe5f7
Karthik Amaram <amaramkart09@gmail.com>
2025-06-03 09:09:01 -0500

    Finished the lesson

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentNum) => (total + currentNum), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentNum) => (total * currentNum), 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let value = 1;
+  for (let i = 1; i <= num; i++) {
+      value *= i;    
+  }
+  return value;
 };
 
 // Do not edit below this line
